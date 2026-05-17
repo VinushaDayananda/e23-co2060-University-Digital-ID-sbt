3468a871a91ecbf9468467e9f7a972869cc8f674
VinushaDayananda <e23054@eng.pdn.ac.lk>
2026-05-17 12:01:07 +0530

    Implemented Soulbound ERC721 and manager contract

---
+++ code/blockchain/contracts/PeraSoul.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.6.0
+pragma solidity ^0.8.24;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+/// @custom:security-contact e23054@eng.pdn.ac.lk
+contract PeraSoul is ERC721, ERC721Burnable, Ownable {
+    uint256 private nextTokenId;
+
+    mapping(address=>bool) public hasToken;
+    mapping(address=>uint256) public studentToken;
+
+    event TokenIssued(address indexed _student, uint256 indexed tokenId);
+    event TokenBurned(address indexed _student, uint256 indexed tokenId);
+
+    modifier validStudent(address _student) {
+        require(_student!=address(0),"Invalid Student Address"); 
+        _;
+    }
+
+    constructor()
+        ERC721("PeraSoul", "PERA")
+        Ownable(msg.sender)
+    {
+        nextTokenId=1;
+    }
+
+    function mint(address _student) external onlyOwner validStudent (_student) returns (uint256) {
+        require(hasToken[_student]==false,"Student Already Has SBT");
+
+        uint256 tokenId = nextTokenId;
+        nextTokenId++;
+        hasToken[_student]=true;
+        studentToken[_student]=tokenId;
+
+        _safeMint(_student, tokenId);
+        emit TokenIssued(_student,tokenId);
+        return tokenId;
+
+    }
+
+    function burn(address _student) external onlyOwner validStudent(_student) {
+        require(hasToken[_student],"Token Not Found");
+
+        uint256 tokenId=studentToken[_student];
+        hasToken[_student]=false;
+        delete studentToken[_student];
+        _burn(tokenId);
+        emit TokenBurned(_student,tokenId);
+    }
+
+    function _update(address to, uint256 tokenId,address auth) internal override returns(address) {
+        address from=_ownerOf(tokenId);
+        if (from!=address(0) && to!=address(0)) {
+            revert("SoulBound: transfer is denied");
+        }
+        return super._update(to, tokenId, auth);
+    }
+}
---
+++ code/blockchain/contracts/PeraSoulManager.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IPeraSoul {
+    function mint(address student) external returns (uint256);
+    function burn(address student) external;
+    function hasToken(address student) external view returns (bool);
+    function studentToken(address student) external view returns (uint256);
+}
+
+contract PeraSoulManager is Ownable {
+    IPeraSoul public peraSoul;
+
+    mapping(uint256 => bool) public permanentlyRevoked;
+    mapping(uint256 => uint256) public temporarilyRevokedUntil;
+
+    event StudentTokenMinted(address indexed student, uint256 indexed tokenId);
+    event TokenTemporarilyRevoked(uint256 indexed tokenId, uint256 untilTime);
+    event TokenPermanentlyRevoked(uint256 indexed tokenId);
+    event StudentWalletReplaced(address indexed oldWallet,address indexed newWallet,uint256 oldTokenId,uint256 newTokenId);
+
+    constructor(address peraSoulAddress) Ownable(msg.sender) {
+        require(peraSoulAddress != address(0), "Invalid PeraSoul address");
+        peraSoul = IPeraSoul(peraSoulAddress);
+    }
+
+    function mintStudentToken(address student) external onlyOwner returns (uint256) {
+        uint256 tokenId = peraSoul.mint(student);
+
+        emit StudentTokenMinted(student, tokenId);
+
+        return tokenId;
+    }
+
+    function revokeTemporarily(address student, uint256 durationInSeconds) external onlyOwner {
+        require(peraSoul.hasToken(student), "Student has no token");
+        require(durationInSeconds > 0, "Invalid duration");
+
+        uint256 tokenId = peraSoul.studentToken(student);
+
+        temporarilyRevokedUntil[tokenId] = block.timestamp + durationInSeconds;
+
+        emit TokenTemporarilyRevoked(tokenId, temporarilyRevokedUntil[tokenId]);
+    }
+
+    function revokePermanently(address student) external onlyOwner {
+        require(peraSoul.hasToken(student), "Student has no token");
+
+        uint256 tokenId = peraSoul.studentToken(student);
+
+        permanentlyRevoked[tokenId] = true;
+
+        peraSoul.burn(student);
+
+        emit TokenPermanentlyRevoked(tokenId);
+    }
+
+    function verifyStudent(address student) external view returns (bool) {
+        if (!peraSoul.hasToken(student)) {
+            return false;
+        }
+
+        uint256 tokenId = peraSoul.studentToken(student);
+
+        if (permanentlyRevoked[tokenId]) {
+            return false;
+        }
+
+        if (block.timestamp < temporarilyRevokedUntil[tokenId]) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function getRemainingRevocationTime(address student) external view returns (uint256) {
+        require(peraSoul.hasToken(student), "Student has no token");
+
+        uint256 tokenId = peraSoul.studentToken(student);
+        uint256 revokedUntil = temporarilyRevokedUntil[tokenId];
+
+        if (block.timestamp >= revokedUntil) {
+            return 0;
+        }
+
+        return revokedUntil - block.timestamp;
+    }
+
+    function replaceStudentWallet(address oldWallet, address newWallet)
+    external
+    onlyOwner
+    returns (uint256)
+{
+    require(oldWallet != address(0), "Invalid old wallet");
+    require(newWallet != address(0), "Invalid new wallet");
+    require(oldWallet != newWallet, "Wallets must be different");
+
+    require(peraSoul.hasToken(oldWallet), "Old wallet has no token");
+    require(!peraSoul.hasToken(newWallet), "New wallet already has token");
+
+    uint256 oldTokenId = peraSoul.studentToken(oldWallet);
+
+    // Burn old token
+    peraSoul.burn(oldWallet);
+
+    // Mint new token
+    uint256 newTokenId = peraSoul.mint(newWallet);
+
+    emit StudentWalletReplaced(oldWallet, newWallet, oldTokenId, newTokenId);
+
+    return newTokenId;
+}
+}
